dde9368724168ab9601ab967d210a6edcca55154
Alexander Tukai <tukai@alutech-mc.com>
2014-09-25 11:28:20 +0000

    Add translator messages

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,17 @@
 <?php
 
 return array(
+    'translator' => [
+        'locale' => 'ru_RU',
+        'translation_file_patterns' => [
+            [
+                'type'     => 'phparray',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.php',
+            ],
+        ],
+    ],
+
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
---
+++ language/ru_RU.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'Dashboard'    => 'Дашборд',
+    'Summary'      => 'Сводка',
+
+    'Settings'     => 'Настройки',
+    'General'      => 'Общие',
+
+    'Create'       => 'Создать',
+    'Save'         => 'Сохранить',
+
+    'Back to list' => 'Назад к списку'
+);
