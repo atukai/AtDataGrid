8fe432095a88bf20e044f86e78c59e3446235c10
Alexander Tukai <tukai@alutech-mc.com>
2015-05-04 15:47:44 +0000

    update js

--- public/js/at-datagrid/datagrid.js
+++ public/js/at-datagrid/datagrid.js
@@ -1,43 +1,29 @@
-var DataGrid = new Object({
-    checkAll: function(sender) {
-        $('.at-grid table').find('.grid-table-checker').attr('checked', sender.checked);
-    },
+var DataGrid = {
 
-    action: function(action, msg, callback) {
-        var checkers = $('.at-grid table input[name="items[]"]:checked');
-
-        if (checkers.length > 0) {
-            msg = msg || '';
-
-            if (msg != '' && !confirm(msg)) {
-                $('#datagrid-list-form').get(0).reset();
-                return false;
-            } else {
-                var form = $('#datagrid-list-form');
-                if (typeof callback != 'undefined') {
-                    callback(form, action, msg);
-                } else {
-                    $('#datagrid-list-form-action').val(action);
-                    form.submit();
-                }
-            }
-        } else {
-            alert('Check one or more rows');
-        }
-    },
-
-    ajaxAction: function(url, callback) {
-        $.ajax({
-            type: 'GET',
-            url: url,
-            cache: true,
-            dataType: 'html',
-            success: callback
+    init: function () {
+        $('#at-datagrid-check-all').click(function(e) {
+            that = this;
+            $('.grid-table-checker').each(function() {
+                this.checked = that.checked;
+            });
         });
     },
 
-    confirmAction: function(sender, message) {
+    confirmAction: function(element, message) {
+        event = event || window.event;
+        event.preventDefault();
+
         message = message || "Are you sure?";
-        return confirm(message)
+        var location = $(element).attr('href') || '';
+
+        bootbox.confirm(message, function(result) {
+            if (result) {
+                window.location.replace(location);
+            }
+        });
     }
+};
+
+$(document).ready(function () {
+    DataGrid.init();
 });
