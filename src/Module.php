877cf4a8f155aaefd2b43c14685c2f6eeac4381b
Alexander Tukai <tukai@alutech-mc.com>
2016-10-26 14:55:35 +0300

    Fix sm config

--- src/Module.php
+++ src/Module.php
@@ -2,21 +2,10 @@
 
 namespace AtDataGrid;
 
-use AtDataGrid\View\Helper\QueryParams;
-
 class Module
 {
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
-    }
-
-    public function getViewHelperConfig()
-    {
-        return [
-            'invokables' => [
-                'queryParams' => QueryParams::class,
-            ],
-        ];
     }
 }
